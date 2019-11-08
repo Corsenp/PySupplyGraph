7f41535702e12ca845d4fbe9e4d956e36f5f136c
Corsenac Pierre <pierre.corsenac@epitech.eu>
2019-11-08 13:09:23 -0500

    [PYSUPPLYGRAPH] [MATPLOTLIB] : adding the creation of the graph

--- pysupplygraph.py
+++ pysupplygraph.py
@@ -1,14 +1,37 @@
 import csv
 import sys
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+# Create the graph
+def create_graph(price, q_demand, q_supplied):
+    try:
+        plt.plot(price, q_demand, price, q_supplied)
+        plt.ylabel('Price')
+        plt.show()
+    except:
+        print('ERROR : can\'t create graph')
 
 # Open Csv file
 def open_csv_file(csv_name):
 
     try:
+        price = []
+        q_demand = []
+        q_supplied = []
         with open(csv_name, 'r') as csv_file:
             csv_reader = csv.reader(csv_file)
             for line in csv_reader:
-                print(line)
+                line[0] = line[0].strip()
+                line[1] = line[1].strip()
+                line[2] = line[2].strip()
+                if line[0].strip().isnumeric() == True and line[1].isnumeric() == True and line[2].isnumeric() == True:
+                    price.append(int(line[0]))
+                    q_demand.append(int(line[1]))
+                    q_supplied.append(int(line[2]))
+        create_graph(price, q_demand, q_supplied)
+
     except:
         print("ERROR : couldn't open file")
        
