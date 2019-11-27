dbf9a4aee203b5d609ae5ae77eb04edb173c467a
Corsenac Pierre <pierre.corsenac@epitech.eu>
2019-11-27 17:47:38 -0500

    [SCRIPT] [LIST] [NAME] [PRICES] : replacing list price with list prices because there is multiple price in it

--- pysupplygraph.py
+++ pysupplygraph.py
@@ -5,9 +5,9 @@ import numpy as np
 
 
 # Create the graph
-def create_graph(price, q_demand, q_supplied):
+def create_graph(prices, q_demand, q_supplied):
     try:
-        plt.plot(price, q_demand, price, q_supplied)
+        plt.plot(prices, q_demand, prices, q_supplied)
         plt.ylabel('Price')
         plt.show()
     except:
@@ -25,7 +25,7 @@ def clean_line(string):
 def open_csv_file(csv_name):
 
     try:
-        price = []
+        prices = []
         q_demand = []
         q_supplied = []
         with open(csv_name, 'r') as csv_file:
@@ -35,10 +35,10 @@ def open_csv_file(csv_name):
                 line[1] = clean_line(line[1])
                 line[2] = clean_line(line[2])
                 if line[0].strip().isnumeric() == True and line[1].isnumeric() == True and line[2].isnumeric() == True:
-                    price.append(int(line[0]))
+                    prices.append(int(line[0]))
                     q_demand.append(int(line[1]))
                     q_supplied.append(int(line[2]))
-        create_graph(price, q_demand, q_supplied)
+        create_graph(prices, q_demand, q_supplied)
 
     except:
         print("ERROR : couldn't open file")
